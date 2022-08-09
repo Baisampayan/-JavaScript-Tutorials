deb3269c54b41cedec028c56f3896f9ab91e020f
Baisampayan <baisampayandas@gmail.com>
2022-08-09 16:47:33 +0530

    Added: string search() method

--- String Methods/program.js
+++ String Methods/program.js
@@ -28,3 +28,7 @@ console.log(str5.lastIndexOf("S"));     // Position of the Character "S" in the 
 console.log(str5.lastIndexOf("F",1));  // Position of the Character "e" in the string after the 13th index
 console.log(str5.lastIndexOf("Development"));  // Position of the String "Full" in the string
 console.log(str5.lastIndexOf("Full",39));  // Position of the Character "e" in the string after the 13th index
+
+// String Method search()
+const str6 = "Full Stack Development at Full Speed";
+console.log(str6.search("at")); // Return the position of the "at" in the string
